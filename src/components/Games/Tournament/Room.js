23c52e7d8efab811c47ca8de584698bc78692983
Easiv <easiv89@gmail.com>
2020-11-02 22:55:13 +0100

    user gets star for being a host

--- src/components/Games/Tournament/Room.js
+++ src/components/Games/Tournament/Room.js
@@ -8,19 +8,22 @@ export default function Room() {
       id: '1234',
       code: 'XD3217',
 			name: 'Pokój świrków',
-			host: 'idName',
+			host: 'Szczepan',
       playerList: [
 				{
 					id: 1,
-					name: 'Szczepan'
+          name: 'Szczepan',
+          host: true
 				},
 				{
 					id: 2,
-					name: 'Seba'
+          name: 'Seba',
+          host: false
 				},
 				{
 					id: 3,
-					name: 'Cziter'
+          name: 'Cziter',
+          host: false
 				},
 			],
 			generatedQuestions: [
@@ -34,22 +37,22 @@ export default function Room() {
     }
 	}
 	return (
-		<div className="main-view">
+		<div className="room-view">
       
-			<div className="tournament-logo">
-        <div>Tournament</div>
+			{/* <div className="tournament-logo">
+        <h1>Tournament</h1>
         <hr/>
         <div>Lista Graczy</div>
-      </div>
-      
-      <div className="room-code">
-        {data.room.id}
-      </div>
-      
+      </div> */}
+
       <div className="player-list">
+        <h2>Lista Graczy</h2>
         <PlayerList players={data.room.playerList}/>
 			</div>
       
+      <div className="room-code">
+        <h1>{data.room.code}</h1>
+      </div>
 		</div>
 	)
 }
--- src/components/Games/Tournament/Room/PlayerList.js
+++ src/components/Games/Tournament/Room/PlayerList.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { styled } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import FaceIcon from '@material-ui/icons/Face';
+import StarIcon from '@material-ui/icons/Star';
 
 export default function PlayerList(props) {
+
+  const PlayerCard = styled(ListItem)({
+    background: 'white',
+    padding: '10px',
+    width: '300px'
+  })
   return (
     <div className="main-list">
       <List component="nav">
         {props.players.map(p => (
-          <ListItem divider="true">
+          <PlayerCard divider="true">
             <ListItemIcon>
               <FaceIcon />
             </ListItemIcon>
             <ListItemText key={p.id} primary={p.name} />
-          </ListItem>
+            {p.host === true && <StarIcon style={{color: 'yellow', fontSize: '30px'}}/>}
+          </PlayerCard>
         ))}
       </List> 
     </div>
